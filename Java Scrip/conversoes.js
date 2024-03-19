1b3380919429eee20cfec6924e657374808b23b3
NathanLG7 <143437707+NathanLG7@users.noreply.github.com>
2024-03-19 15:10:46 -0300

    Add files via upload

---
+++ Java Scrip/conversoes.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// convsoes implicita
+const numero = 456;
+const numeroString = Number ("456 "); 
+
+//number()
+//String()
+console.log(numero + Number(numeroString))
+
+// conversao esplicita
---
+++ Java Scrip/type-number.js
@@ -0,0 +1,19 @@
+// 1º
+
+const meuNumro = 3;
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+
+const operacaoMatematica =  primeiroNumero + segundoNumero;
+
+console. log(operacaoMatematica) 
+
+// 2º
+const x = 3.3;
+const y = .5;
+
+const z = x / y;
+
+console. log(z)
+
